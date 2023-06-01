d9a24a4c51dacc671cc7d9f7fc0fcef245f02834
SudevaHarsha <sudevaharsha1@gmail.com>
2023-06-01 13:10:32 +0530

    home page,admin page completed

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,8 @@ import CreateProduct from './pages/Admin/CreateProduct';
 import Users from './pages/Admin/users';
 import Orders from './pages/user/Orders';
 import Profile from './pages/user/Profile';
+import Products from './pages/Admin/Products';
+import UpdateProduct from './pages/Admin/UpdateProduct';
 
 
 
@@ -33,7 +35,9 @@ function App() {
       <Route path="/dashboard" element={<AdminRoute/>}>
          <Route path="admin" element={<AdminDashboard/>}></Route>
          <Route path="admin/create-category" element={<CreateCategory/>}></Route>
-         <Route path="admin/create-product" element={<CreateProduct/>}></Route>
+         {<Route path="admin/create-product" element={<CreateProduct/>}></Route>}
+         {<Route path="admin/product/:slug" element={<UpdateProduct/>}></Route>}
+         <Route path="admin/products" element={<Products/>}></Route>
          <Route path="admin/users" element={<Users/>}></Route>
       </Route>
       <Route path="/register" element={<Register/>}></Route>
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import { isAdmin, requireSignin } from '../middlewares/authMiddleware.js';
-import { createProductController } from '../controllers/productController.js';
+import { createProductController, deleteProductController, getProductController, getSingleProductController, productCountController, productFiltersController, productListController, productPhotoController, updateProductController } from '../controllers/productController.js';
 import formidable from 'express-formidable';
 const router=express.Router();
 router.post('/create-product',requireSignin,isAdmin,formidable(),createProductController)
+
+router.get('/get-product', getProductController)
+router.get('/get-product/:slug', getSingleProductController)
+router.get('/product-photo/:pid',productPhotoController)
+router.delete('/product/:pid', deleteProductController)
+router.put('/update-product/:pid',requireSignin,isAdmin,formidable(),updateProductController)
+
+router.post('/product-filters',productFiltersController)
+router.get('/product-count',productCountController)
+router.get('/product-list/:page',productListController)
 
 export default router;
