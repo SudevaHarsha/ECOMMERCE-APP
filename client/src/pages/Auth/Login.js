38d18b3c824fa2428b9fd97f92daf9119891a835
SudevaHarsha <sudevaharsha1@gmail.com>
2023-09-10 17:58:13 +0530

    s

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -11,6 +11,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [auth,setAuth] = useAuth();
+  
 
   const navigate = useNavigate();
   const location=useLocation();
