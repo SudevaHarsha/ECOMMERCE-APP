cfb95f48394d2ee5c7f18cec89401e98b8f7854b
SudevaHarsha <sudevaharsha1@gmail.com>
2023-09-15 22:00:34 +0530

    error corrected, finished

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ import categoryRoutes from './routes/categoryRoute.js';
 import productRoutes from './routes/productRoute.js'
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 const app = express();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.use(cors());
 app.use(express.json());
