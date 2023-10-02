9b171372ff920b0c0d3f58a9d014057b341c0df6
SudevaHarsha <sudevaharsha1@gmail.com>
2023-10-02 09:51:19 +0530

    last

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ import { fileURLToPath } from 'url';
 
 dotenv.config();
 const app = express();
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+/* const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename) */
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, './client/build')))
+/* app.use(express.static(path.join(__dirname, './client/build'))) */
 
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/category',categoryRoutes);
@@ -27,9 +27,9 @@ app.use('/api/v1/product',productRoutes);
 
 
 
-app.use('*' ,function(req,res){
+/* app.use('*' ,function(req,res){
   res.sendFile(path.join(__dirname, './client/build/index.html'))
-})
+}) */
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT,()=>{
